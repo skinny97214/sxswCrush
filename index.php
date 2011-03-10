cfb0b4e957a7bd3d0850d89f4af1a68915a98cb2
Crystal Beasley <crystalb@skinnys-mac-2.local>
2011-03-10 15:56:26 -0800

    twitter error styling and GA tracking

--- index.php
+++ index.php
@@ -23,7 +23,19 @@
 		});
 	});
 </script>
+<script type="text/javascript">
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-21976013-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 <body>
 
@@ -54,7 +66,7 @@ if (isset($result)) {
 	} elseif ($result == '403') {
 		print "<h3 class='error'>Oops! That was a duplicate tweet.</h3>";
 	} else {
-		print "<h3 class='error'>Oh noes! We have no idea what happened, but it didn't work.</h3>";
+		print "<h3 class='error'>" . $result . "Y U NO WORK, TWITTER?</h3>";
 	}
 }
 
